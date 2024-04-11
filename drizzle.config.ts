c5e9adc749dba3dcb0ee8f79cfdf2e45097dd058
pulkitgangwar <pulkitgangwar2016@gmail.com>
2024-04-11 13:08:25 +0530

    made schema file as server file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  schema: "./src/lib/schema.ts",
+  schema: "./src/lib/schema.server.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
