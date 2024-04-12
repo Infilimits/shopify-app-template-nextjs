67cbf41a27fec0af910074417429664bdbf12e55
pulkitgangwar <pulkitgangwar2016@gmail.com>
2024-04-12 09:47:24 +0530

    added resolver to resolve @ import alias for nextjs build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  webpack(config) {
+    const __dirname = new URL(".", import.meta.url).pathname;
+
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
   env: {
     PUBLIC_SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
   },
